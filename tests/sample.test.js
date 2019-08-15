ad49d73d9c3e9b90d5607677c6d33bcf69eb7209
v123582 <v123582@gmail.com>
2019-08-15 15:34:19 +0800

    加入更多的條件 #2

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -5,4 +5,7 @@ describe('sample', () => {
   it('# add', () => {
     expect(add(1, 2)).to.equal(3)
   })
+  it('# 參數包含非數字', () => {
+    expect(() => { add('a', 'b') }).to.throw('參數必須為數字')
+  })
 }) 
