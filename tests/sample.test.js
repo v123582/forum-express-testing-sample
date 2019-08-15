d0400be65641afcf6a4a6cbecba5cc9037ae268e
v123582 <v123582@gmail.com>
2019-08-15 19:53:05 +0800

    加入更多的條件

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -5,4 +5,8 @@ describe('sample', () => {
   it('# add', () => {
     expect(add(1, 2)).to.equal(3)
   })
+
+  it('# isNaN(a) || isNaN(b)', () => {
+    expect(() => { add('a', 'b') }).to.throw('參數必須為數字')
+  })
 })
